c1c46c33a89cca8c92f04755a041a4d9f50bce94
Osemen Silas <osemensilas@gmail.com>
2026-04-02 17:49:28 +0100

    update Brands.tsx

--- components/general/Brands.tsx
+++ components/general/Brands.tsx
@@ -3,6 +3,57 @@ import Image from "next/image";
 import { useEffect, useState } from "react";
 
 const Brands = () => {
+    
+    type brand = {
+        id: int,
+        name: string,
+        image: string 
+    }
+    
+    const [brands, setBrands] = useState<brand[]>([]);
+    
+    const ourBrands = [
+        {
+            id: 1,
+            name: "Purina",
+            image: "brand1.jpg"
+        },
+        {
+            id: 2,
+            name: "Purina",
+            image: "brand1.jpg"
+        },
+        {
+            id: 3,
+            name: "Purina",
+            image: "brand1.jpg"
+        },
+        {
+             id: 4,
+             name: "Purina",
+             image: "brand1.jpg"
+         },
+        {
+             id: 5,
+             name: "Purina",
+             image: "brand1.jpg"
+         },
+        {
+             id: 6,
+             name: "Purina",
+             image: "brand1.jpg"
+         },
+        {
+             id: 7,
+             name: "Purina",
+             image: "brand1.jpg"
+         },
+        {
+             id: 8,
+             name: "Purina",
+             image: "brand1.jpg"
+         },
+    ];
     return ( 
         <>
         <section className="h-max w-screen px-10 py-10">
@@ -11,16 +62,15 @@ const Brands = () => {
                 <Link href="/all-best-sellers" className="text-primary text-base font-medium hover:underline">View All</Link>
             </div>
             <div className="h-max w-full flex flex-wrap items-center gap-3 justify-between">
-                <Link href="/shop-by-brand?brand=brand1" className="h-max w-max flex">
+                {
+                    ourBrands.map((brand, index) => (
+                                        <Link href="/shop-by-brand?brand=brand1" key={index} className="h-max w-max flex">
                     <div className="relative h-[150px] w-[180px]">
                         <Image src={"/pl1.jpg"} alt="best seller 1" fill />
                     </div>
                 </Link>
-                <Link href="/shop-by-brand?brand=brand2" className="h-max w-max flex">
-                    <div className="relative h-[150px] w-[180px]">
-                        <Image src={"/pl2.jpg"} alt="best seller 2" fill />
-                    </div>
-                </Link>
+                    ))
+                }
             </div>
         </section>
         </>
