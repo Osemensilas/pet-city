f8e767e893e298ac720294ce244322c3d9177977
osemensilas <osemensilas@gmail.com>
2026-04-02 20:47:24 +0100

    update

--- app/page.tsx
+++ app/page.tsx
@@ -117,10 +117,10 @@ const HomePage = () => {
     </section>
     <section className="h-[70vh] w-full px-4 sm:px-10 py-10">
       <div className="h-full w-full flex rounded bg-accent">
-        <div className="relative w-full sm:w-1/2 h-full hidden sm:block">
+        <div className="relative w-1/2 h-full hidden sm:block">
           <Image src={"/pet-doc.jpg"} fill alt="pharmacy image" className="rounded-tl rounded-bl" />
         </div>
-        <div className="w-1/2 h-full flex justify-center flex-col gap-10 sm:px-10 px-4">
+        <div className="w-full sm:w-1/2 h-full flex justify-center flex-col gap-10 sm:px-10 px-4">
             <h2 className="text-3xl font-semibold">Health Matters</h2>
             <p className="">Take care of your pet health with our medication and keep them healthy to ensure a long, happy, and active life.</p>
             <div className="">
@@ -136,7 +136,7 @@ const HomePage = () => {
           <h2 className="text-3xl font-semibold">Newsletter</h2>
           <p className="">Want to get discount, special offer to to make your pet happy? Join our newsletter!</p>
           <form className="h-max w-full">
-            <input className="h-10 w-[75%] border border-grey px-5 outline-none" placeholder="Enter email address" type="text" name="email" id="email" />
+            <input className="h-10 w-full sm:w-[75%] border border-grey px-5 outline-none" placeholder="Enter email address" type="text" name="email" id="email" />
             <div className="h-max w-full mt-5">
               <button type="button" className="bg-primary text-background px-5 py-2 rounded hover:bg-primary-dark transition-colors duration-300">Join</button>
             </div>
