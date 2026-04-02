f952bb59559e8fe3f8c16302ae6352c9fa100fea
osemensilas <osemensilas@gmail.com>
2026-04-02 19:37:06 +0100

    update BestSellers.tsx

--- components/general/BestSellers.tsx
+++ components/general/BestSellers.tsx
@@ -2,7 +2,7 @@
 
 import Link from "next/link";
 import Image from "next/image";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
 const BestSeller = () => {
 
