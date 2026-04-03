77c19e6273dc6bc69a88b159f3e959a653f294d1
osemensilas <osemensilas@gmail.com>
2026-04-03 18:41:08 +0100

    update page.tsx

--- app/contact-us/page.tsx
+++ app/contact-us/page.tsx
@@ -1,7 +1,17 @@
 const Page = () => {
   return(
     <>
-      
+    <section className="h-[40vh] w-full flex justify-center items-center">
+      <h1>Contact Us</h1>
+    </section>
+    <section className="h-max w-max flex flex-col sm:flex-row py-10 px-4 sm:px-10">
+      <div className="h-max w-full sm:w-1/2">
+        
+      </div>
+      <div className="h-max w-full sm:w-1/2">
+        
+      </div>
+    </section>
     </>
   );
 }
