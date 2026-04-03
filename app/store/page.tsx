62e6ba6453e2646e7f347be9882c7fa2416a4f3b
osemensilas <osemensilas@gmail.com>
2026-04-03 15:02:39 +0100

    update page.tsx

--- app/store/page.tsx
+++ app/store/page.tsx
@@ -218,16 +218,16 @@ const Page = () => {
                   ourProducts.map((product, index) => (
                     <div key={index} className="h-max w-max bg-accent rounded p-3">
                       <div className="relative h-[150px] w-[180px]">
-                          <Image src={"/best1.jpg"} alt="best seller 1" fill />
+                          <Image src={product.image} alt="best seller 1" fill />
                       </div>
                       <div className="w-[180px] h-[100px] mt-2 pb-2">
                           <div className="h-max w-full flex items-center justify-between">
-                              <h3 className="text-base font-semibold">Hills</h3>
-                              <p>In stock</p>
+                              <h3 className="text-base font-semibold">{product.name}</h3>
+                              <p>{product.stock}</p>
                           </div>
                           <h3 className="text-base font-semibold">Science Diet</h3>
                           <div className="w-full h-max pt-2 flex items-center justify-between">
-                              <span className="text-text text-base font-bold">₦{(5000).toLocaleString()}</span>
+                              <span className="text-text text-base font-bold">₦{(product.price).toLocaleString()}</span>
                               <button onClick={() => viewProduct(product.productId)} type="button" className="border-primary border text-primary px-3 py-1 rounded hover:bg-primary hover:text-background transition-colors duration-300 ml-5 text-sm"><i className="fa fa-arrow-right"></i> <span> Add</span></button>
                           </div>
                       </div>
