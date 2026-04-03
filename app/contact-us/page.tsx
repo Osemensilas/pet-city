d2d7c2e55edf8491cf094fd9ae1b2b11768fd236
osemensilas <osemensilas@gmail.com>
2026-04-03 19:29:04 +0100

    update page.tsx

--- app/contact-us/page.tsx
+++ app/contact-us/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 const Page = () => {
   return(
     <>
@@ -33,11 +35,22 @@ const Page = () => {
         </div>
       </div>
       <div className="h-max w-full sm:w-1/2">
-        <form className="" onSubmit={(e) => e.preventDefauld}>
-          <h2 className="">Get in Touch</h2>
-          <p className="">We would love to hear from you. please, fill out the form and out team will get back to you as soon as possible.</p>
-          <div className="">
-            
+        <form className="h-max w-full sm:w-[350px] bg-accent box-shadow rounded p-10" onSubmit={(e) => e.preventDefault()}>
+          <h2 className="text-3xl font-bold mb-10 text-center">Get in Touch</h2>
+          <p className="text-base mb-5">We would love to hear from you. please, fill out the form and out team will get back to you as soon as possible.</p>
+          <div className="h-max w-full">
+            <div className="h-max w-full">
+              <input type="text" className="text-base px-5 h-10 w-full border rounded outline-none border-grey" name="fullname" placeholder="Fullname" />
+            </div>
+            <div className="h-max w-full">
+              <input type="text" className="text-base px-5 h-10 w-full border rounded outline-none border-grey" name="email" placeholder="Email Address" />
+            </div>
+            <div className="h-max w-full">
+              <textArea type="text" className="text-base px-5 max-h-[150px] min-h-[150px] max-w-full min-w-full border rounded outline-none border-grey" name="message"placeholder="Your Message" ></textArea>
+            </div>
+          </div>
+          <div className="h-max w-full py-5 mt-5 border-t border-grey">
+            <button className="w-full rounded bg-primary py-2 text-center text-background" type="submit">Send Message</button>
           </div>
         </form>
       </div>
