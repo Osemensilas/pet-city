ef3b8d73e80626ec677dc4d52182cfab9f925224
osemensilas <osemensilas@gmail.com>
2026-04-04 11:56:51 +0100

    update page.tsx

--- app/product/page.tsx
+++ app/product/page.tsx
@@ -21,7 +21,22 @@ const Page = () => {
             </div>
           </div>
           <div className="h-max sm:w-[35%] w-full bg-accent rounded p-4 sm:p-10">
-            
+            <p className="px-4 py-2 bg-green rounded">In Stock</p>
+            <h1 className="text-3xl text-header mt-4">Purina Pro Plan</h1>
+            <p className="text-text text-base mt-4">Science Diet</p>
+            <p className="text-text text-base mt-4">Items Left: 26</p>
+            <div className="h-max w-full flex items-center gap-3">
+              <button type="button" className="h-10 w-10 rounded bg-primary flex items-center justify-centet text-background text-base">
+                <p>-</p>
+              </button>
+              <input className="h-10 w-20 border border grey rounded px-5" value="1" />
+              <button type="button" className="h-10 w-10 rounded bg-primary flex items-center justify-centet text-background text-base">
+                <p>+</p>
+              </button>
+            </div>
+            <div className="h-max w-full mt-4">
+              <button className="h-max w-full py-2 text-center bg-primary text-background text-base">Add to Cart</button>
+            </div>
           </div>
         </div>
         <div className=""></div>
