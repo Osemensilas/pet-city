e8a4760117367815894db4218c1a2941160cdfdd
osemensilas <osemensilas@gmail.com>
2026-04-05 09:11:40 +0100

    update page.tsx

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -3,6 +3,31 @@
 import Image from "next/image";
 
 const Page = () => {
+    
+    type User = {
+        fullname: string;
+        email: string;
+        password: string;
+    }
+    
+    const [error, setError] = useState("");
+    const [formData, setFormData] = useState({
+        email: "",
+        password: ""
+    });
+    
+    const handleChanged = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        
+        setFormData({...formData, [name]: value});
+    }
+    
+    const btnClicked = () => {
+        if (!formData.email || !formData.password){
+            setError("All field required");
+        }
+    }
+    
     return ( 
         <>
         <section className="login h-sreen w-screen flex">
@@ -12,21 +37,23 @@ const Page = () => {
             <div className="w-full sm:w-1/2 h-full flex justify-center items-center">
                 <form className="w-full sm:w-[450px] bg-accent rounded px-10" onSubmit={(e) => e.preventDefault()}>
                     <h2 className="text-text text-3xl font-bold text-center py-5">Client Login</h2>
+                    <div className={`h-max w-full bg-danger px-2 py-2 ${error ? "" : "hidden"}
+                    `}>{error}</div>
                     <div className="h-max w-full border-t border-grey pt-5">
                         <div className="h-max w-full flex flex-col">
                             <label htmlFor="email" className="text-grey mb-2 text-base">Email:</label>
-                            <input type="text" id="email" className="h-10 w-full rounded border border-grey outline-none px-5" />
+                            <input type="text" name="email" value={formData.email} onChange={handleChanged} id="email" className="h-10 w-full rounded border border-grey outline-none px-5" />
                         </div>
                         <div className="h-max w-full flex flex-col">
                             <label htmlFor="password" className="text-grey mb-2 text-base">Password:</label>
-                            <input type="password" id="password" className="h-10 w-full rounded border border-grey outline-none px-5" />
+                            <input type="password" name="password" value={formData.password} onChange={handleChanged} id="password" className="h-10 w-full rounded border border-grey outline-none px-5" />
                         </div>
                     </div>
                     <div className="h-max w-full mt-5">
                         Don't have an account? <a href="/register" className="text-primary hover:underline">Register</a>
                     </div>
                     <div className="h-max w-full py-5 mt-5 border-t border-grey">
-                        <button className="w-full rounded bg-primary py-2 text-center text-background" type="submit">Login</button>
+                        <button className="w-full rounded bg-primary py-2 text-center text-background" type="submit" onClick={btnClicked} >Login</button>
                     </div>
                     <div className="h-max w-full my-3">
                         <a href="/forget-password" className="text-primary hover:underline">Forgot Password?</a>
