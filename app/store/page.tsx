4c3a28e927caaa3fcf72d2f6773c58067e9fa9fb
osemensilas <osemensilas@gmail.com>
2026-04-06 08:07:07 +0100

    update page.tsx

--- app/store/page.tsx
+++ app/store/page.tsx
@@ -135,7 +135,7 @@ const Page = () => {
             </button>
           </div>
           <div className="h-max w-full bg-accent rounded p-2 sm:p-4">
-            <div className="h-max w-full flex items-center flex-wrap gap-3 mb-5">
+            <div className="h-max w-full flex items-center flex-wrap gap-3 mb-5 justify-between">
               {
                 allProducts.length < 1 ? (
                   <div className="h-screen w-full flex items-center justify-center">
